a3fc4994ccaa7c73fb80d4ee4eaed462a7161476
David Estrich <destrich@evergage.com>
2018-12-04 23:11:49 -0500

    Update to use compoennts on most parts of app

--- client/main.js
+++ client/main.js
@@ -4,59 +4,19 @@ import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
 
 import {Players} from './../imports/api/players';
-
-const renderPlayers = (players) => {
-   return players.map((player) => {
-      return (
-        <p key={player._id}>
-          {player.name} has {player.score} point(s). 
-          
-          <button onClick={() => {
-            Players.update( player._id, { $inc: { score: 1 } });
-            }}>+1</button>
-
-          <button onClick={() => {
-            Players.update( player._id, { $inc: { score: -1} });
-          }}>-1</button>
-          
-          <button onClick={() => Players.remove(player._id)}>X</button>
-        </p>
-     );
-   });
-}
-
-const handleSubmit = (e) => {
-  let playerName = e.target.playerName.value;
-
-  e.preventDefault();
-
-  if (playerName) {
-    e.target.playerName.value = '';
-    Players.insert({
-      name: playerName,
-      score: 0
-    });
-  }
-}
+import TitleBar from './../imports/ui/TitleBar';
+import AddPlayer from '../imports/ui/AddPlayer';
+import PlayerList from './../imports/ui/PlayerList';
 
 Meteor.startup(() => {
-
-  let title = 'Score Keeper'
-  let name = 'David';
-  
   Tracker.autorun(() => {
     let players = Players.find().fetch();
+    let title = "Score Keeper";
     let jsx = (
       <div>
-        {/* render h1 tag with title vat */}
-        <h1>{title}</h1>
-        <p>Hello {name}!</p>
-        <p>This is my second p</p>
-        {renderPlayers(players)}
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="playerName" placeholder="Player Name"></input>
-          <button>Add Player</button>
-        </form>
+        <TitleBar title={title} subtitle="Created by David Estrich"/>
+        <PlayerList players={players}/>
+        <AddPlayer score={0}/>
       </div>
     );
     ReactDOM.render(jsx, document.getElementById('app'));
--- server/main.js
+++ server/main.js
@@ -2,5 +2,62 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 
 Meteor.startup(() => {
+    class Person {
+        constructor (name = 'Anon', age = 99) {
+            this.name = name;
+            this.age = age
+        }
+        getGreeting() {
+            return `Hi I am ${this.name}`
+        }
+        getPersonDescription() {
+            return `${this.name} is ${this.age} year(s) old.`
+
+        }
+    }
+
+    class Employee extends Person {
+        constructor (name, age, title) {
+            super(name, age);
+            this.title = title;
+        }
+        getGreeting() {
+            if (this.title) {
+                return `Hi I am ${this.name}. I work as a ${this.title}.`
+            } else {
+                return super.getGreeting();
+            }
+        }
+        hasJob() {
+            return !!this.title
+        }
+    }
+
+    class Programmer extends Person {
+        constructor (name, age, language = 'assembly') {
+            super(name, age);
+            this.language = language;
+        }
+        getGreeting() {
+            if (this.language) {
+                return `Hi I am ${this.name}. My favorite language is ${this.language}.`
+            } else {
+                return super.getGreeting();
+            }
+        }
+    }
+
+    let me = new Employee("Dave", 24, "Solutions Engineer");
+    console.log(me.getGreeting());
+
+    let employee = new Employee("Liz", 23, "UX Researcher");
+    console.log(employee.getPersonDescription());
+
+    let programmer = new Programmer("Connor", 26, "Javascript");
+    console.log(programmer.getGreeting());
+
+    let dan = new Programmer();
+    console.log(dan.getGreeting());
+
 
 });
